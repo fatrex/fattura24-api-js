1e558a1b9413ab9f569d44cdf604d9eec4f81f05
Daniele Lenares <daniele.lenares@gmail.com>
2020-03-17 09:39:01 +0100

    feat: add test api key method

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+const axios = require('axios')
+
+const APIError = require('./classes/APIError')
+const APIResponse = require('./classes/APIResponse')
+
 class Fattura24API {
-  // TODO To Implement
+  constructor (apiKey, apiVersion = '0.3') {
+    if (!apiKey) throw new Error('apiKey is missing!')
+    this.apiKey = apiKey
+    this.apiVersion = apiVersion
+
+    this.$axios = axios.create({
+      baseURL: `https://www.app.fattura24.com/api/v${this.apiVersion}`
+    })
+  }
+
+  async testKey () {
+    try {
+      const response = await this.$axios.post('/TestKey', {
+        apiKey: this.apiKey
+      })
+      return new APIResponse(response)
+    } catch (error) {
+      throw new APIError(error.toString())
+    }
+  }
 }
 
 module.exports = Fattura24API
