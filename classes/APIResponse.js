31f96347c4e99e7fe3b7eebaeb3100ed9c881fe5
Daniele Lenares <daniele.lenares@gmail.com>
2020-03-17 17:02:08 +0100

    feat: add response api parser

--- classes/APIResponse.js
+++ classes/APIResponse.js
@@ -1,24 +1,24 @@
 const XMLParser = require('fast-xml-parser')
+const HTMLEntities = require('html-entities').AllHtmlEntities
+const htmlEntities = new HTMLEntities()
+
+const APIError = require('./APIError')
 
 class Fattura24APIResponse {
   constructor ({ data, error }) {
-    this.rawResponse = data
-    this.rawError = error
+    this.rawResponse = data || error
+    return this.__parse(data)
   }
 
-  toJSON () {
-    return this.__parse(this.rawResponse)
-  }
-
-  toString () {
-    const { description } = this.__parse(this.rawResponse)
-    return description
-  }
-
-  __parse (data) {
+  __parse (rawData) {
     try {
-      const { root: parsed } = XMLParser.parse(data)
-      return parsed
+      const { root: parsed } = XMLParser.parse(rawData)
+      const { returnCode, description, ...data } = parsed
+      if (returnCode < 0) throw new APIError(description, returnCode)
+      return {
+        message: htmlEntities.decode(description),
+        data
+      }
     } catch (error) {
       console.error(error)
     }
